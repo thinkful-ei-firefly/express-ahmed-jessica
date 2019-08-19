196cd6de31cd30f3be1a5d8a81d0646bb9019f47
Jess Doyle <jessica.e.doyle@gmail.com>
2019-08-19 15:56:48 -0400

    p3

--- index.js
+++ index.js
@@ -31,6 +31,49 @@ app.get('/cipher', (req, res) => {
   res.end(`${code}`);
 });
 
+app.get('/lotto', (req, res) => {
+  let numbers = req.query.arr;
+  numbers = numbers.map(val => parseInt(val));
+  console.log(numbers);
+  const winningNums =[];
+  while (winningNums.length<6) {
+    const newNum = Math.ceil(Math.random()*20);
+    if (!winningNums.includes(newNum)) {
+      winningNums.push(newNum);
+    }
+  }
+  console.log(winningNums);
+
+  let score = 0;
+  for (let i=0; i<numbers.length; i++) {
+    console.log(numbers[i]);
+    console.log(winningNums.includes(numbers[i]));
+    if (winningNums.includes(numbers[i])) {
+      score+=1;
+    }
+  }
+  console.log(score);
+
+  let response;
+
+  switch (score) {
+  case 4:
+    response = 'Congratulations, you win a free ticket!';
+    break;
+  case 5:
+    response = 'Congratulations! You win $100!';
+    break;
+  case 6:
+    response = 'Wow! Unbelieveable! You could have won the mega millions!';
+    break;
+  default:
+    response = 'Sorry, you lose.';
+    break;    
+  }
+
+  res.end(response);
+});
+
 
 app.listen(8000, () =>{
   console.log('Express server listening on port 8000!');
